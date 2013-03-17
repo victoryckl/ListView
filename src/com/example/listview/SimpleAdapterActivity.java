9e1b73cca57a189ff99e5cf123978b225e25fb2d
victroyckl <victoryckl2012@gmail.com>
2013-03-17 15:55:17 +0800

    添加按钮，"Add"和"Delete" #	modified:   res/layout/activity_simple_adapter.xml #	modified:   src/com/example/listview/SimpleAdapterActivity.java

--- src/com/example/listview/SimpleAdapterActivity.java
+++ src/com/example/listview/SimpleAdapterActivity.java
@@ -7,8 +7,11 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 public class SimpleAdapterActivity extends Activity {
 	private ListView mList;
@@ -20,17 +23,48 @@ public class SimpleAdapterActivity extends Activity {
 
 		mList = (ListView) findViewById(R.id.simplelist);
 
-		SimpleAdapter adapter = new SimpleAdapter(this, getData(),
-				R.layout.simple_item, new String[] { "text" },
+		List<Map<String, Object>> listdata = getData();
+		setListAdapter(listdata);
+		
+		findViewById(R.id.add_item).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				List<Map<String, Object>> listdata = (List<Map<String, Object>>)mList.getTag();
+				int count = listdata.size();
+				listdata.add(getOneItem("item " + count));
+				setListAdapter(listdata);
+			}
+		});
+		
+		findViewById(R.id.delete_item).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				List<Map<String, Object>> listdata = (List<Map<String, Object>>)mList.getTag();
+				int count = listdata.size();
+				if (count > 0) {
+					listdata.remove(count - 1);
+					setListAdapter(listdata);
+				} else {
+					Toast.makeText(getBaseContext(), "no item!", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+	}
+	
+	private void setListAdapter(List<Map<String, Object>> listdata) {
+		SimpleAdapter adapter = new SimpleAdapter(
+				getBaseContext(), 
+				listdata,
+				R.layout.simple_item, 
+				new String[] { "text" },
 				new int[] { R.id.text });
 		mList.setAdapter(adapter);
+		mList.setTag(listdata);
 	}
-
-	private int mCount = 100;
 
 	private List<Map<String, Object>> getData() {
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < mCount; i++) {
+		for (int i = 0; i < 10; i++) {
 			list.add(getOneItem("item " + i));
 		}
 		return list;
