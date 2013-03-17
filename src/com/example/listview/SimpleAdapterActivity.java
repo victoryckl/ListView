87200b2282512b2623b69d5957cf49f822985d50
victroyckl <victoryckl2012@gmail.com>
2013-03-17 11:25:29 +0800

    code format modified:   src/com/example/listview/SimpleAdapterActivity.java

--- src/com/example/listview/SimpleAdapterActivity.java
+++ src/com/example/listview/SimpleAdapterActivity.java
@@ -12,31 +12,33 @@ import android.widget.SimpleAdapter;
 
 public class SimpleAdapterActivity extends Activity {
 	private ListView mList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_simple_adapter);
-		
-		mList = (ListView)findViewById(R.id.simplelist);
-		
-        SimpleAdapter adapter = new SimpleAdapter(this, getData(),
-                R.layout.simple_item, new String[] { "text" },
-                new int[] { R.id.text });
-        mList.setAdapter(adapter);
+
+		mList = (ListView) findViewById(R.id.simplelist);
+
+		SimpleAdapter adapter = new SimpleAdapter(this, getData(),
+				R.layout.simple_item, new String[] { "text" },
+				new int[] { R.id.text });
+		mList.setAdapter(adapter);
 	}
 
 	private int mCount = 100;
+
 	private List<Map<String, Object>> getData() {
-        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        for (int i = 0; i < mCount; i++) {
-        	list.add(getOneItem("item " + i));
-        }
-        return list;
-    }
-	
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		for (int i = 0; i < mCount; i++) {
+			list.add(getOneItem("item " + i));
+		}
+		return list;
+	}
+
 	private Map<String, Object> getOneItem(String text) {
 		Map<String, Object> map = new HashMap<String, Object>();
-        map.put("text", text);
-        return map;
+		map.put("text", text);
+		return map;
 	}
 }
